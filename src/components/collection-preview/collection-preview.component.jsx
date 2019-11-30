34987cbc441e164a938bdedeb53d57d2fcf2eba7
Michael Eskarous <mneskarous@gmail.com>
2019-11-29 19:32:30 -0800

    Created CollecttionItem and CollectionPreview components

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import CollectionItem from '../collection-item/collection-item.component.jsx'
+
 import './collection-preview.styles.scss';
+
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
@@ -9,8 +12,8 @@ const CollectionPreview = ({ title, items }) => (
       {
         items
         .filter((item, idx) => idx < 4)
-        .map((item) => (
-          <div key={item.id}>{item.name}</div>
+        .map(({ id, ...otherItemProps } ) => (
+          <CollectionItem key={id} { ...otherItemProps } />
         ))
       }
     </div>
