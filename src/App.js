5305938feb2814ffc9140af168cd41efebef3945
Michael Eskarous <mneskarous@gmail.com>
2019-12-01 16:45:02 -0800

    Implemented authentication with sign in with email and password

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default class App extends Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          }, () => {console.log(this.state)});
+          });
         });
       }
       this.setState({
