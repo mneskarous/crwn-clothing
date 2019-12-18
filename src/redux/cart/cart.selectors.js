4b4db6620806890df97084ee579f9bab089d1675
Michael Eskarous <mneskarous@gmail.com>
2019-12-18 15:22:10 -0800

    Implemented toggle hidden to cart dropdown menu when button is clicked

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -15,9 +15,19 @@ export const selectCartHidden = createSelector(
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems => 
-  cartItems.reduce(
-    (accumulatedQuantity, cartItem) => 
-      accumulatedQuantity + cartItem.quantity, 
-    0
-  )
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) => 
+        accumulatedQuantity + cartItem.quantity, 
+      0
+    )
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems => 
+    cartItems.reduce(
+      (accumulatedPrice, cartItem) => 
+        accumulatedPrice + (cartItem.quantity * cartItem.price),
+      0
+    )
 );
