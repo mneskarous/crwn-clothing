9e5fa909e9a7bf7b871376d85428a33023dbba4b
Michael Eskarous <mneskarous@gmail.com>
2019-12-21 10:32:25 -0800

    Created CheckoutItem Component and imported it into Checkout component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
+import { 
+  selectCartItems, 
+  selectCartTotal 
+} from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
 
@@ -27,7 +32,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     </div>
     {
       cartItems.map(cartItem => (
-        cartItem.name
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))
     }
   <div className="total">
